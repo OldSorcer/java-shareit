7f8be413a18982a0a6e7990566494122e01eea97
Роман Рахипов <roman.zer@yandex.ru>
2022-10-26 18:19:45 +0300

    fix: исправлена валидация в классе BookingValidator

--- gateway/src/main/java/ru/practicum/shareit/booking/validator/BookingValidator.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/validator/BookingValidator.java
@@ -4,8 +4,11 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 
 public class BookingValidator {
     public static void validateBooking(BookingDto booking) {
-        if (booking.getEnd().isBefore(booking.getStart()) || !booking.getEnd().isAfter(booking.getStart())) {
+        if (booking.getEnd().isBefore(booking.getStart())) {
             throw new IllegalArgumentException("Некорректное время начала бронирования");
+        }
+        if (!booking.getStart().isBefore(booking.getEnd()) && booking.getEnd().isAfter(booking.getStart())) {
+            throw new IllegalArgumentException("Время начала и окончания бронирования не должно совпадать.");
         }
     }
 }
