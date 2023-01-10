b912918a0c649b1cd2adfa944771dc88ba9274ec
Роман Рахипов <roman.zer@yandex.ru>
2023-01-10 11:27:42 +0300

    "Исправлена валидация времени начала и окончания бронирования"

--- gateway/src/main/java/ru/practicum/shareit/booking/validator/BookingValidator.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/validator/BookingValidator.java
@@ -4,11 +4,8 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 
 public class BookingValidator {
     public static void validateBooking(BookingDto booking) {
-        if (booking.getEnd().isBefore(booking.getStart())) {
+        if (!booking.getEnd().isAfter(booking.getStart())) {
             throw new IllegalArgumentException("Некорректное время начала бронирования");
-        }
-        if (!booking.getStart().isBefore(booking.getEnd()) && !booking.getEnd().isAfter(booking.getStart())) {
-            throw new IllegalArgumentException("Время начала и окончания бронирования не должно совпадать.");
         }
     }
 }
